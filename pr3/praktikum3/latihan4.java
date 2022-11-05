587c196e4b377e92bb777aa9c7df6182b5793e34
Gaidahaudatuljannah <117494887+Gaidahaudatuljannah@users.noreply.github.com>
2022-11-05 16:16:20 +0800

    Add files via upload

---
+++ pr3/praktikum3/latihan4.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.praktikum3;
+import java.util.Scanner;
+/**
+ *
+ * @author defaultuser0
+ */
+public class latihan4 {
+    public static void main(String[] args){
+        Scanner scan = new Scanner(System.in).useDelimiter("\n");
+        System.out.println("apakah anda mahasiswa ITS Mandiri, Gaida Haudatul Jannah?(Y jika setuju)");
+        String jawaban = scan.next();
+        
+        if ("Y".equals(jawaban)){
+            System.out.println("selamat berjuang di ITS Mandiri Gida Haudatul Jannah");
+        } else{
+                    System.out.println("Selamat datang di ITS Mandiri, Gaida Haudatul Jannah");
+                    }
+                    
+                    }
+        }
+    
+
+
